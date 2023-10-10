40362d1a20c93e7a09481202b85ca7fb0b869a38
Gustavo R. Conforti <56703129+gustavoconforti@users.noreply.github.com>
2023-10-10 15:14:15 -0300

    Add syslog server as destination feature

--- http_json_logger.py
+++ http_json_logger.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 import json
 import os
 import logging
+import socket
 from flask_talisman import Talisman
 
 app = Flask(__name__)
@@ -12,8 +13,13 @@ app = Flask(__name__)
 log_file = '/var/log/http-json-logger/requests.log'
 logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(message)s')
 
+# Configure the syslog server
+udp_ip = '192.168.0.1'
+udp_port = 514
+
 # Define the expected fields
-expected_fields = ["request_method", "host", "status"]
+expected_fields = ["data_source", "original_timestamp", "event"]
+
 
 # Configure Talisman with security headers
 csp = {
@@ -45,7 +51,7 @@ def log_json():
         # Check for the custom authentication header and value
         auth_header = request.headers.get('X-Auth-Token')
         if auth_header is None or auth_header != 'test_token':
-            return jsonify({"error": "Authentication failed"}), 401  # Unauthorized
+            return jsonify({"error": "Authentication failed"}), 401
 
         # Check if the request contains JSON data
         if request.is_json:
@@ -57,17 +63,33 @@ def log_json():
                 with open(log_file, 'a') as log:
                     log.write(json.dumps(data) + '\n')
 
-                return jsonify({"message": "JSON data logged successfully"}), 200
+                # Send JSON data to the UDP server
+                send_to_udp_server(data)
+
+                return jsonify({"message": "Success"}), 200
             else:
-                return jsonify({"error": "Invalid JSON data"}), 400
+                return jsonify({"error": "Error"}), 400
         else:
-            return jsonify({"error": "Invalid JSON data"}), 400
+            return jsonify({"error": "Error"}), 400
     except Exception as e:
-        return jsonify({"error": "Invalid JSON data"}), 400  # Return a generic error for unhandled exceptions
+        return jsonify({"error": "Error"}), 400
 
 def validate_data(data):
     # Check if data contains only the expected fields
     return set(data.keys()) == set(expected_fields)
+
+def send_to_udp_server(data):
+    try:
+        # Create a UDP socket
+        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        # Send the JSON data to the UDP server
+        udp_socket.sendto(json.dumps(data).encode(), (udp_ip, udp_port))
+
+        # Close the socket
+        udp_socket.close()
+    except Exception as e:
+        print("Error sending data to UDP server:", str(e))
 
 if __name__ == '__main__':
     # Ensure the log file exists
